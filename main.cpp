2432a9f7ab842a51ab0b14d6dabaa1cf38403bb1
Shantanu Sinha <sinha93@purdue.edu>
2022-10-22 02:07:57 -0400

    TELL ME WHY I NEVER WANNA HEAR YOU SAY

--- main.cpp
+++ main.cpp
@@ -6,29 +6,24 @@
 #include <map>
 
 #include "Instruction.h"
+#include "token.cpp"
+#include "opcodes.h"
 
-using namespace std;
-
-int main()
+int main(int argc, char** argv)
 {
-    ifstream fptr("OutputAndTestCases/OutputAndTestCases/TestCases10_08_22/1PushI");
+    std::ifstream fptr(argv[1]);
 
     if (!fptr.is_open()) {
-        cout << "Error opening file" << endl;
+        std::cout << "Error opening file" << std::endl;
         return EXIT_FAILURE;
     }
 
-    string instr = "";
-    queue<Instruction *> instr_queue;
-    std::string opcode, op1, op2, jumpaddr;
-
-    
-
-
+    std::string instr;
+    std::queue <Instruction> instr_queue;
 
     while (getline(fptr, instr)) {
-        tie(opcode, op1, op2, jumpaddr) = parse_text(instr);
-        instr_queue.push(new Instruction(opcode, op1, op2, jumpaddr));
+        std::cout << instr << std::endl;
+        instr_queue.push (get_instruction (instr));
     }
 
     fptr.close();
@@ -36,8 +31,16 @@ int main()
     return EXIT_SUCCESS;
 }
 
-tuple<string, string, string, string> parse_text(string instr)
-{
-    string opcode, op1, op2, jumpaddr;
-    return make_tuple(opcode, op1, op2, jumpaddr);
+
+Instruction get_instruction (std::string ins) {
+    Instruction instr;
+    Token t (ins);
+    t.tokenize();
+
+    if (t.op1 == "start") {
+        instr.setOpcode(OP_START_PROGRAM);
+        
+
+
+    return instr;
 }
--- token.cpp
+++ token.cpp
@@ -2,92 +2,121 @@
 #define _TOKEN_H_
 
 #include <cstring> 
+#include <iostream>
 #include "Instruction.h"
 
-using namespace std;
-
-class token {
-    public:
+class Token {
+    private:
         int i = 0;
-        string op1 = "/0";
-        string op2 = "/0";
-        string op3 = "/0";
-        string op4 = "/0";
-        string op5 = "/0";
+        void set_op (char* s);
+    
+    public:
+        Token (std::string str);
+        void print_tokens () const;
 
-        void set_op (char* s) {
-            string str(s);
-            i++;
-            if (i == 1) {
-                op1 = str;
-            }
+        void tokenize (std::string s);
+        void tokenize ();
 
-            else if (i == 2) {
-                op2 = str;
-            }
-
-            else if (i == 3) {
-                op3 = str;
-            }
-
-            else if (i == 4) {
-                op4 = str;
-            }
-
-            else {
-                op5 = str;
-            }
-        }
+        std::string op1 = "/0";
+        std::string op2 = "/0";
+        std::string op3 = "/0";
+        std::string op4 = "/0";
+        std::string op5 = "/0";
+        std::string op6 = "/0";
+        std::string op7 = "/0";
+        std::string inst = "/0";
+        
+    protected:
+        void set_instruction (std::string ins);
 };
 
-token tokenizer (string instruction) {
-    char *ptr, *str;
-    str = &instruction[0];
-    ptr = strtok(str, " , ");
+Token::Token (std::string str) {
+    Token::inst = str;
+}
 
-    token t;
-    while (ptr != NULL)  
-    {
-        t.set_op(ptr);
-        ptr = strtok (NULL, " , ");
-    }
-
-    return t;
+void Token::set_instruction (std::string ins) {
+    Token::inst = ins;
 }
 
 
+void Token::set_op (char* s) {
+    std::string str(s);
+    i++;
+    if (i == 1) {
+        op1 = str;
+    }
+
+    else if (i == 2) {
+        op2 = str;
+    }
+
+    else if (i == 3) {
+        op3 = str;
+    }
+
+    else if (i == 4) {
+        op4 = str;
+    }
+
+    else if (i == 5) {
+        op5 = str;
+    }
+
+    else if (i == 6) {
+        op6 = str;
+    }
+
+    else {
+        op7 = str;
+    }
+}
+
+void Token::print_tokens() const {
+    std::cout << "op1: " << op1 << std::endl;
+    std::cout << "op2: " << op2 << std::endl;
+    std::cout << "op3: " << op3 << std::endl;
+    std::cout << "op4: " << op4 << std::endl;
+    std::cout << "op5: " << op5 << std::endl;
+    std::cout << "op6: " << op6 << std::endl;
+    std::cout << "op7: " << op7 << std::endl;
+}
+
+void Token::tokenize (std::string s) {
+    set_instruction (s);
+    tokenize ();
+}
+
+void Token::tokenize () {
+    char *ptr, *str;
+    str = &inst[0];
+    ptr = strtok(str, " , ");
+
+    while (ptr != NULL)  
+    {
+        set_op(ptr);
+        ptr = strtok (NULL, " , ");
+    }
+}
+
+#endif
+
 /* Uncomment to test tokenizer
-#include <iostream>
 int main () {
-    string st = "JumpZero, FIBL0, (18)";
-    string st2 = "PushScalar A, (0)";
-    string st3 = "Prints 2+6=";
+    std::string st = "JumpZero, FIBL0, (18)";
+    std::string st2 = "PushScalar A, (0)";
+    std::string st3 = "Prints 2+6=";
 
-    token t1 = tokenizer (st);
-    
-    cout << t1.op1 <<endl;
-    cout << t1.op2 <<endl;
-    cout << t1.op3 <<endl;
-    cout << t1.op4 <<endl;
-    cout << t1.op5 <<endl;
+    Token t1(st);
+    Token t2(st2);
+    Token t3(st3);
 
-    token t1 = tokenizer (st2);
-    
-    cout << t1.op1 <<endl;
-    cout << t1.op2 <<endl;
-    cout << t1.op3 <<endl;
-    cout << t1.op4 <<endl;
-    cout << t1.op5 <<endl;
+    t1.tokenize();
+    t2.tokenize();
+    t3.tokenize();
 
-    token t1 = tokenizer (st3);
-    
-    cout << t1.op1 <<endl;
-    cout << t1.op2 <<endl;
-    cout << t1.op3 <<endl;
-    cout << t1.op4 <<endl;
-    cout << t1.op5 <<endl;
+    t1.print_tokens();
+    t2.print_tokens();
+    t3.print_tokens();
     return 0;
 }
 */
-
-#endif
