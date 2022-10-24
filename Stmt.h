69983fbd43361d79797390a2f9f9074338113588
Ronit Nallagatla <ronit.nallagatla@gmail.com>
2022-10-24 00:18:24 -0400

    shantanu pls learn git

--- Stmt.h
+++ Stmt.h
@@ -1,12 +1,35 @@
 #ifndef STMT_H_
 #define STMT_H_
 
+#include "opcodes.h"
+
 class Stmt {
 private:
-    int opcode;
+    uint32_t opcode;
 
 public:
     virtual void serialize() = 0;
+};
+
+// class Declscal : public Stmt {
+// public:
+//     void serialize();
+// };
+
+// class Declarr : public Stmt {
+// public:
+//     void serialize();
+// };
+
+class Label : public Stmt {
+public:
+    void serialize();
+};
+
+class Gosublabel : public Stmt {
+
+public:
+    void serialize();
 };
 
 #endif /* STMT_H_ */
