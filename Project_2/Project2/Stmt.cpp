77a75e0fbf261b279f0c146c698f5d75590bb2e1
Ronit Nallagatla <ronit.nallagatla@gmail.com>
2022-10-26 17:48:49 -0400

    added data structs

--- Project_2/Project2/Stmt.cpp
+++ Project_2/Project2/Stmt.cpp
@@ -3,130 +3,130 @@
 
 void gosublabel::serialize()
 {
-    std::cout << "gosublabel" << std::endl;
+    std::cout << "GoSubLabel" << std::endl;
 }
 
 void label::serialize()
 {
-    std::cout << "label" << std::endl;
+    std::cout << "Label" << std::endl;
 }
 
 void pushi::serialize()
 {
-    std::cout << "pushi" << std::endl;
+    std::cout << "PushI" << std::endl;
 }
 
 void declscal::serialize()
 {
-    std::cout << "declscal" << std::endl;
+    std::cout << "DeclScalar" << std::endl;
 }
 
 void declarr::serialize()
 {
-    std::cout << "declarr" << std::endl;
+    std::cout << "DeclArray" << std::endl;
 }
 
 void start::serialize()
 {
-    std::cout << "start" << std::endl;
+    std::cout << "Start" << std::endl;
 }
 
 void end::serialize()
 {
-    std::cout << "end" << std::endl;
+    std::cout << "End" << std::endl;
 }
 
 void Exit::serialize()
 {
-    std::cout << "exit" << std::endl;
+    std::cout << "Exit" << std::endl;
 }
 
 void jump::serialize()
 {
-    std::cout << "jump" << std::endl;
+    std::cout << "Jump" << std::endl;
 }
 
 void jumpzero::serialize()
 {
-    std::cout << "jumpzero" << std::endl;
+    std::cout << "Jumpzero" << std::endl;
 }
 
 void jumpnzero::serialize()
 {
-    std::cout << "jumpnzero" << std::endl;
+    std::cout << "JumpNZero" << std::endl;
 }
 
 void gosub::serialize()
 {
-    std::cout << "gosub" << std::endl;
+    std::cout << "GoSub" << std::endl;
 }
 
 void Return::serialize()
 {
-    std::cout << "return" << std::endl;
+    std::cout << "Return" << std::endl;
 }
 
 void pushscal::serialize()
 {
-    std::cout << "pushscal" << std::endl;
+    std::cout << "PushScalar" << std::endl;
 }
 
 void pusharr::serialize()
 {
-    std::cout << "pusharr" << std::endl;
+    std::cout << "PushArray" << std::endl;
 }
 
 void pop::serialize()
 {
-    std::cout << "pop" << std::endl;
+    std::cout << "Pop" << std::endl;
 }
 
 void popscal::serialize()
 {
-    std::cout << "popscal" << std::endl;
+    std::cout << "PopScalar" << std::endl;
 }
 
 void poparr::serialize()
 {
-    std::cout << "poparr" << std::endl;
+    std::cout << "PopArray" << std::endl;
 }
 
 void dup::serialize()
 {
-    std::cout << "dup" << std::endl;
+    std::cout << "Dup" << std::endl;
 }
 
 void swap::serialize()
 {
-    std::cout << "swap" << std::endl;
+    std::cout << "Swap" << std::endl;
 }
 
 void add::serialize()
 {
-    std::cout << "add" << std::endl;
+    std::cout << "Add" << std::endl;
 }
 
 void negate::serialize()
 {
-    std::cout << "negate" << std::endl;
+    std::cout << "Negate" << std::endl;
 }
 
 void mul::serialize()
 {
-    std::cout << "mul" << std::endl;
+    std::cout << "Mul" << std::endl;
 }
 
 void Div::serialize()
 {
-    std::cout << "div" << std::endl;
+    std::cout << "Div" << std::endl;
 }
 
 void printtos::serialize()
 {
-    std::cout << "printtos" << std::endl;
+    std::cout << "PrintTOS" << std::endl;
 }
 
 void prints::serialize()
 {
-    std::cout << "prints" << std::endl;
+    std::cout << "Prints" << std::endl;
 }
--- Project_2/Project2/TableEntry.h
+++ Project_2/Project2/TableEntry.h
@@ -3,15 +3,22 @@
 
 #include <string>
 
-class TableEntry {
-private:
-    std::pair<double, double> entry;
+// class TableEntry {
+// private:
+//     std::pair<double, double> entry;
 
+// public:
+//     TableEntry(double loc, double len);
+//     double getLoc();
+//     double getLen();
+//     std::pair<double, double> getEntry();
+// };
+
+class TableEntry {
 public:
+    double loc;
+    double len;
     TableEntry(double loc, double len);
-    double getLoc();
-    double getLen();
-    std::pair<double, double> getEntry();
 };
 
 #endif /* TABLE_ENTRY_H_ */
