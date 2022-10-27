b33bc09d2d792d8d0f61295e38927a38061a4e87
Shantanu Sinha <sinha93@purdue.edu>
2022-10-27 12:51:30 -0400

    Changed Directory Structure. SymbolTable Lookups not working, PLus label values wrong

---
+++ Deprecated_Stuff/Project2_Backup/SymbolTable.h
@@ -0,0 +1,31 @@
+#ifndef SYMBOLTABLE_H_
+#define SYMBOLTABLE_H_
+
+#include <string>
+#include <iostream>
+#include <map>
+#include "TableEntry.h"
+
+class SymbolTable {
+private:
+    SymbolTable();
+    static SymbolTable* instance;
+    int size;
+    int scope;
+    std::map<std::string, TableEntry> map;
+    std::map<std::string, TableEntry> subMap;
+
+public:
+    static SymbolTable* getInstance();
+    void addEntry(std::string key, TableEntry entry);
+    void addLabel(std::string key, int loc);
+    void addVar(std::string key, int len);
+    void addSubEntry(std::string key, TableEntry entry);
+    TableEntry getEntry(std::string key);
+    TableEntry getSubEntry(std::string key);
+    int getLoc();
+    void setScope(int scope);
+    int getNumVar(int scope);
+};
+
+#endif /* SYMBOLTABLE_H_ */
---
+++ Deprecated_Stuff/Project2_Backup/main.cpp
@@ -0,0 +1,164 @@
+#include <fstream>
+#include <iostream>
+#include <queue>
+
+#include "SymbolTable.h"
+#include "Token.h"
+#include "opcodes.h"
+
+Instruction* get_instruction(std::string instr);
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cout << "Error: Invalid number of arguments" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string instr;
+    std::ifstream fptr(argv[1]);
+    std::vector <Instruction*> instr_queue;
+
+    while (getline(fptr, instr)) {
+        Instruction* instr_ptr = get_instruction(instr);
+        if (instr_ptr != nullptr) {
+            instr_queue.push_back(instr_ptr);
+        }
+    }
+    
+    for (auto instr : instr_queue) {
+        instr->serialize();
+    }
+    
+    fptr.close();
+}
+
+Instruction* get_instruction(std::string instr)
+{
+    SymbolTable* symbolTable = SymbolTable::getInstance();
+    Token t(instr);
+    Instruction* ins = nullptr;
+
+    t.tokenize();
+
+    if (t.inst == "declscal") {
+        ins = new declscal();
+        ins->label_for_symbol_table = t.op1;
+        symbolTable->addVar(t.op1, 1);
+    }
+
+    else if (t.inst == "declarr") {
+        ins = new declarr();
+        ins->label_for_symbol_table = t.op1;
+        ins->length = stoi(t.op2);
+        symbolTable->addVar(t.op1, stoi(t.op2));
+    }
+
+    else if (t.inst == "label") {
+        symbolTable->addLabel(t.op1, symbolTable->getLoc());
+    }
+
+    else if (t.inst == "gosublabel") {
+        ins = new gosublabel();
+        ins->label_for_symbol_table = t.op1;
+        symbolTable->addLabel(t.op1, symbolTable->getLoc());
+    }
+
+    else if (t.inst == "start") {
+        ins = new start();
+    }
+
+    else if (t.inst == "end") {
+        ins = new End();
+    }
+
+    else if (t.inst == "exit") {
+        ins = new Exit();
+    }
+
+    else if (t.inst == "jump") {
+        ins = new jump();
+        ins->label_for_symbol_table = t.op1;
+        symbolTable->addVar(t.op1, 0);
+    }
+
+    else if (t.inst == "jumpzero") {
+        ins = new jumpzero(t.op1);
+        symbolTable->addVar(t.op1, 0);
+    }
+
+    else if (t.inst == "jumpnzero") {
+        ins = new jumpnzero(t.op1);
+        symbolTable->addVar(t.op1, 0);
+    }
+
+    else if (t.inst == "gosub") {
+        ins = new gosub(t.op1);
+        symbolTable->setScope(1);
+        symbolTable->addVar(t.op1, 0);
+    }
+
+    else if (t.inst == "return") {
+        ins = new Return();
+    }
+
+    else if (t.inst == "pushscal") {
+        ins = new pushscal(t.op1);
+        symbolTable->addVar(t.op1, 1);
+    }
+
+    else if (t.inst == "pusharr") {
+        ins = new pusharr(t.op1);
+        symbolTable->addVar(t.op1, 1);
+    }
+
+    else if (t.inst == "pushi") {
+        ins = new pushi(stoi(t.op1));
+    }
+
+    else if (t.inst == "pop") {
+        ins = new pop();
+    }
+
+    else if (t.inst == "popscal") {
+        ins = new popscal(t.op1);
+    }
+
+    else if (t.inst == "poparr") {
+        ins = new poparr(t.op1);
+    }
+
+    else if (t.inst == "dup") {
+        ins = new dup();
+    }
+
+    else if (t.inst == "swap") {
+        ins = new Swap();
+    }
+
+    else if (t.inst == "add") {
+        ins = new add();
+    }
+
+    else if (t.inst == "negate") {
+        ins = new Negate();
+    }
+
+    else if (t.inst == "mul") {
+        ins = new mul();
+    }
+
+    else if (t.inst == "div") {
+        ins = new Div();
+    }
+
+    else if (t.inst == "printtos") {
+        ins = new printtos();
+    }
+
+    else if (t.inst == "prints") {
+        ins = new prints(t.op1);
+    }
+
+    return ins;
+}
