64fd657ab693185780a51dc929adb73dc400117f
Ronit Nallagatla <ronit.nallagatla@gmail.com>
2022-11-17 17:41:37 -0500

    schtuff

--- Step2/VM_idea_forgot_to_push.cpp
+++ Step2/VM_idea_forgot_to_push.cpp
@@ -24,8 +24,17 @@ int main(int argc, char** argv)
     RuntimeStack* runtimeStack = RuntimeStack::getInstance();
     StringBuffer* stringBuffer = StringBuffer::getInstance();
 
-    while (getline(fptr, instrline, 'Start ')) {
+    while (getline(fptr, instrline)) {
+        Token instr(instrline);
+        instr.tokenize();
+        if (instr.inst == "Start") {
+            break;
+        }
         stringBuffer->add(instrline);
+    }
+
+    while (getline(fptr, instrline)) {
+        operate(instrline);
     }
 
     stringBuffer->printDebug();
@@ -33,49 +42,49 @@ int main(int argc, char** argv)
     return EXIT_SUCCESS;
 }
 
-// void operate(std::string instr)
-// {
-//     Token t = Token(instr);
+void operate(std::string instr)
+{
+    Token t = Token(instr);
 
-//     // std::stack is an object available in the standard library
-//     //  Stack* stack = Stack::getInstance(); // Make stack singleton
-//     RuntimeStack* stack = RuntimeStack::getInstance();
+    // std::stack is an object available in the standard library
+    //  Stack* stack = Stack::getInstance(); // Make stack singleton
+    RuntimeStack* stack = RuntimeStack::getInstance();
 
-//     if (t.inst == "Add") {
-//         stack->push(stack->pop() + stack->pop()); // Implement Push/Pop functions for stack
-//     }
+    if (t.inst == "Add") {
+        stack->push(stack->pop() + stack->pop()); // Implement Push/Pop functions for stack
+    }
 
-//     if (t.inst == "Mul") {
-//         stack->push(stack->pop() * stack->pop());
-//     }
+    if (t.inst == "Mul") {
+        stack->push(stack->pop() * stack->pop());
+    }
 
-//     if (t.inst == "Div") {
-//         stack->push(stack->pop() / stack->pop());
-//     }
+    if (t.inst == "Div") {
+        stack->push(stack->pop() / stack->pop());
+    }
 
-//     if (t.inst == "Negate") {
-//         stack->push(-stack->pop());
-//     }
+    if (t.inst == "Negate") {
+        stack->push(-stack->pop());
+    }
 
-//     if (t.inst == "Swap") {
-//         int temp = stack->pop();
-//         stack->push(stack->pop());
-//         stack->push(temp);
-//     }
+    if (t.inst == "Swap") {
+        int temp = stack->pop();
+        stack->push(stack->pop());
+        stack->push(temp);
+    }
 
-//     if (t.inst == "Dup") {
-//         stack->push(stack->top());
-//     }
+    if (t.inst == "Dup") {
+        stack->push(stack->top());
+    }
 
-//     if (t.inst == "Pop") {
-//         stack->pop();
-//     }
+    if (t.inst == "Pop") {
+        stack->pop();
+    }
 
-//     if (t.inst == "PushScalar") {
-//         stack->push(t.op1);
-//     }
+    // if (t.inst == "PushScalar") {
+    //     stack->push(t.op1);
+    // }
 
-//     if (t.inst == "PushArray") {
-//         stack->push(t.op1[t.op2]);
-//     }
-// }
+    // if (t.inst == "PushArray") {
+    //     stack->push(t.op1[t.op2]);
+    // }
+}
